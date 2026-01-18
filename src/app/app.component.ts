59dd24152a1d345479d4b01be2c461b2ed2ad634
MohamedBenAhmed55 <mohamed.benahmed@ensi-uma.tn>
2026-01-18 19:55:23 +0100

    Pure & Impure Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,9 +23,9 @@ export class AppComponent {
   ];
 
   onReset(index: number) {
-    // this.historicTemperatures[index] = 18;
-    const newTEmps = [...this.historicTemperatures];
-    newTEmps[index] = 18;
-    this.historicTemperatures = newTEmps;
+    this.historicTemperatures[index] = 18;
+    // const newTemps = [...this.historicTemperatures];
+    // newTemps[index] = 18;
+    // this.historicTemperatures = newTemps;
   }
 }
--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -2,7 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'sort',
-  standalone: true
+  standalone: true,
+  // disables the cache mechanism for this pipe to always reflect changes in the input array, the tradeof is that the pipe will be executed a lot
+  // because with caching we watch the reference of our input value, and if the reference doesn't change we don't re-execute the pipe
+  pure: false
 })
 export class SortPipe implements PipeTransform {
 
